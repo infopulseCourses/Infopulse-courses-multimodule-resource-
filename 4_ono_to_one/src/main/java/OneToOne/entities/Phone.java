905f7e50f0b6df1578553b924f02344585db2a18
mariaTarasenko <mtarasenko@itteam.io>
2016-12-17 15:33:26 +0200

    many_to many hibernate

---
+++ 4_ono_to_one/src/main/java/OneToOne/entities/Phone.java
@@ -0,0 +1,7 @@
+package OneToOne.entities;
+
+/**
+ * Created by mtarasenko on 17.12.16.
+ */
+public class Phone {
+}
---
+++ 4_ono_to_one/src/main/java/OneToOne/entities/User.java
@@ -0,0 +1,60 @@
+package OneToOne.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.*;
+
+@Entity(name="users_with_phones")
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private Phone phone;
+
+    public Phone getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Phone phone) {
+        this.phone = phone;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+
+    public User(){}
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
