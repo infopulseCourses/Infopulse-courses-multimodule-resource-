6561d7ebaa4f37a35bb5c2ef527b0580227d1127
mariaTarasenko <mtarasenko@itteam.io>
2016-12-17 16:10:46 +0200

    many_to many hibernate

---
+++ 4_many_to_many/src/main/java/manyToMany/User.java
@@ -0,0 +1,45 @@
+package manyToMany;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="myusers_bank")
+public class User {
+	   @Id
+	    @GeneratedValue
+	    private Long id;
+	    
+	    @Column(name = "name")
+	    private String name;
+	    
+	    @Column(name = "surname")
+	    private String surname;
+	    
+	    public String getSurname() {
+			return surname;
+		}
+
+
+		public void setSurname(String surname) {
+			this.surname = surname;
+		}
+
+		public User(){}
+
+	    public Long getId() {
+	        return id;
+	    }
+
+	    public void setId(Long id) {
+	        this.id = id;
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+	   
+}
--- Chat/src/main/java/Main.java
+++ Chat/src/main/java/Main.java
@@ -1,40 +1,22 @@
-package OneToOne.entities;
-
 import org.hibernate.SessionFactory;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 
 /**
- * Hello world!
- *
+ * Created by mtarasenko on 17.12.16.
  */
-public class App 
-{
+public class Main {
     public static void main( String[] args )
     {
         SessionFactory sessionFactory = (SessionFactory) Persistence.createEntityManagerFactory( "org.hibernate.tutorial.jpa" );
         EntityManager entityManager = sessionFactory.createEntityManager();
         entityManager.getTransaction().begin();
 
-        Phone phone = new Phone();
-        phone.setValue("+380998877666");
-//        entityManager.getTransaction().commit();
-
-        User user1 = new User();
-
-        user1.setName("Max");
-        user1.setSurname("ivanov");
-        user1.setPhone(phone);
-
-
-        phone.setUser(user1);
-
-        entityManager.persist(phone);
-        entityManager.persist(user1);
 
         entityManager.getTransaction().commit();
-    
+
+
         entityManager.close();
         sessionFactory.close();
     }
